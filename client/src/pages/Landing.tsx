f9acffd243b321d9f1de39d39a53adb125d75048
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-02-08 19:28:48 -0600

    Code clean up

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,9 +1,15 @@
 import "../styles/Landing.css"
+import { Link } from "react-router-dom";
+import "../styles/General.css"
 
 export function Landing(){
 
     return(
         <>
+            <div className="content-container">
+                <h1 className="title">See you again soon!</h1>
+                <button className="logout-btn"><Link to="/login">Logout</Link></button>
+            </div>
             <div className="firework"></div>
             <div className="firework"></div>
             <div className="firework"></div>
