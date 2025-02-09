822104c9c068004261d5ed053d727d54b590c8ec
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-02-09 14:13:16 -0600

    Final debugging

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,14 +1,13 @@
-DROP TABLE IF EXISTS users;
-DROP ROLE IF EXISTS test;
-DROP DATABASE IF EXISTS "PERNUser";
-
-CREATE DATABASE "PERNUser";
 \c PERNUser
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
